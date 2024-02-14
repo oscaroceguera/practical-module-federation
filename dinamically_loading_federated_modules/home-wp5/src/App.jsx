ca2930fe931dcebc6a975fcf40987625b8724637
Oscar E. Oceguera Bibriesca <oscar.oceguera@payclip.com>
2024-02-14 01:26:55 -0700

    Dimamically

---
+++ dinamically_loading_federated_modules/home-wp5/src/App.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+function loadComponent(scope, module) {
+  return () =>
+    window[scope].get(module).then((factory) => {
+      const Module = factory();
+      return Module;
+    });
+}
+
+const useDynamicScript = (args) => {
+  console.log("🚀 ~ useDynamicScript ~ args:", args);
+  const [ready, setReady] = React.useState(false);
+  const [failed, setFailed] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!args.url) {
+      return;
+    }
+
+    const element = document.createElement("script");
+
+    element.src = args.url;
+    element.type = "text/javascript";
+    element.async = true;
+
+    setReady(false);
+    setFailed(false);
+
+    element.onload = () => {
+      console.log(`Dinamic Script Loaded: ${args.url}`);
+      setReady(true);
+    };
+
+    element.onerror = () => {
+      console.log(`Dinamic Script Error: ${args.url}`);
+      setReady(false);
+      setFailed(true);
+    };
+
+    document.head.appendChild(element);
+
+    return () => {
+      console.log(`Dinamic Script Removed: ${args.url}`);
+      document.head.removeChild(element);
+    };
+  }, [args.url]);
+
+  return {
+    ready,
+    failed,
+  };
+};
+
+function System(props) {
+  console.log("🚀 ~ System ~ props:", props);
+  const { ready, failed } = useDynamicScript({
+    url: props.system && props.system.url, // if props.system exist send props.system.url
+  });
+  console.log("ready =>", ready);
+  console.log("failed =>", failed);
+
+  if (!props.system) {
+    return <h2>Not system specified</h2>;
+  }
+
+  if (!ready) {
+    return <h2>Loading dynamic script: {props.system.url}</h2>;
+  }
+
+  if (failed) {
+    return <h2>Failed to load dynamic script: {props.system.url}</h2>;
+  }
+
+  const Component = React.lazy(
+    loadComponent(props.system.scope, props.system.module)
+  );
+
+  return (
+    <React.Suspense fallback="Loading System">
+      <Component />
+    </React.Suspense>
+  );
+}
+
+const App = () => (
+  <div>
+    <System
+      system={{
+        url: "http://localhost:8082/remoteEntry.js",
+        scope: "widget",
+        module: "./Widget",
+      }}
+    />
+    <div>Hi there, I'm React from React.</div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
