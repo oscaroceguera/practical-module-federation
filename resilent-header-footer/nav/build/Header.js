30a400ac555aa5ff42f7bfb8e5874497bd73a49f
Oscar E. Oceguera Bibriesca <oscar.oceguera@payclip.com>
2024-02-16 18:52:14 -0700

    Episode 9

---
+++ resilent-header-footer/nav/build/Header.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var Header = function Header() {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    style: {
+      padding: "1em",
+      background: "green",
+      color: "white",
+      fontSize: "xx-large"
+    }
+  }, "HEADER!");
+};
+var _default = exports["default"] = Header;
